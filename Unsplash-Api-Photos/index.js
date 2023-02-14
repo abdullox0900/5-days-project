2e48fb98a3e3ea387f20215bbb8d1b20be146437
abdullox0900 <abdullox0900@gmail.com>
2023-02-14 16:59:40 +0500

    [ADD]: [TWO DAY]: Project name Unsplash Photos

---
+++ Unsplash-Api-Photos/index.js
@@ -0,0 +1,30 @@
+const elList = document.querySelector(".list");
+
+const API_KEY = "NjXfY5kc8FvYvy8vqWB4pFHwwCQR17TtmkQj9AEiksY";
+const apiUrl = `https://api.unsplash.com/photos/collections?page=1&query=office?client_id=${API_KEY}`;
+
+let loader = `<div class="loader">
+                <img src="./img/631c400b903c03d9-loading-gif-wpfaster.gif" alt="loader-gif">
+            </div>`;
+
+elList.innerHTML = loader;
+
+try {
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+        elList.innerHTML = "";
+        data.map((item) => {
+            const elItem = document.createElement("li");
+            elItem.classList.add(".list__item");
+            elItem.innerHTML = `
+                    <img class="list__item-img" src=${item?.urls?.regular} alt="unsplash-img">
+                    <div class="list__item-name"></div>
+                `;
+            elList.append(elItem);
+        });
+    })
+
+} catch (err) {
+    console.log(`${err} Xato`);
+}
